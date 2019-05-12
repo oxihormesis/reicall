e62b66d786efeed6366574be4bb6096539bdedae
oxihormesis <kevin.graham68@gmail.com>
2019-05-12 09:04:23 -0400

    login user if form is valid

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -8,8 +9,8 @@ def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # then log in here
+            user = form.save()
+            login(request, user)
             return redirect('home')
     else:
         form = UserCreationForm()
@@ -20,6 +21,8 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            user = form.get_user()
+            login(request, user)
             return redirect('home')
     else:
         form = AuthenticationForm()
