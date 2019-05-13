fa66251a1d5c0332a2ed68897db323c6d152fb21
oxihormesis <kevin.graham68@gmail.com>
2019-05-13 09:00:28 -0400

    created landing_view and url, template

--- call_tracking/views.py
+++ call_tracking/views.py
@@ -15,6 +15,11 @@ from .utils import search_phone_numbers, purchase_phone_number
 
 
 # Home page view and JSON views to power the charts
+def landing_view(request):
+    """Renders the landing page"""
+    return render(request, 'landing.html')
+
+
 def home(request):
     """Renders the home page"""
     context = {}
--- twilio_sample_project/urls.py
+++ twilio_sample_project/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from call_tracking.views import home
+from call_tracking.views import home, landing_view
 
 urlpatterns = [
-    url(r'^$', home, name='home'),
+    url(r'^$', landing_view, name='landing')
+    url(r'^username', home, name='home'),
     url(r'^call-tracking', include('call_tracking.urls')),
     url(r'^accounts', include('accounts.urls'), name="accounts"),
 
